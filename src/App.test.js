694b5bd42a70eeacc6ae00310a6908891ee3ba3d
AnnaGolitsyna <110576gav@gmail.com>
2023-08-31 15:15:57 +0300

    testInit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe('main text information', () => {
+
+    test('renders company name and slogan', () => {
+      render(<App />);
+
+      const companyName = screen.getByText(/death wish coffee/i);
+      const companeSlogan = screen.getByText(/world's strongest coffee/i);
+
+      expect(companyName).toBeInTheDocument();
+      expect(companeSlogan).toBeInTheDocument();
+    });
+
+})
+
